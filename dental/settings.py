fa546a5032f85e65a6fadfb4193895f885e75ecb
Nithin Devarajan <nitinkarthick.22@gmail.com>
2022-12-30 12:20:41 +0530

    Adding production host

--- dental/settings.py
+++ dental/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^%7++-c6zu9ir4&w9dro^c#=b=kq9y*3ahr11%3!4#%s@otg3f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangodeployapp-production.up.railway.app','127.0.0.1','localhost']
 
 
 # Application definition
